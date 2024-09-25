beba1ad3176904818e4e152931d94f70ddf731f8
krysthecoder <krysthopher@gmail.comm>
2024-09-25 17:58:46 -0600

    yield: tailwind css, and added utilsData.tsx

---
+++ src/utils/utilsData.tsx
@@ -0,0 +1,6 @@
+// Here is all the content of my website, links, files links, etc.
+
+export const utilsData: object = {
+  devName: 'Krysthopher Ruiz',
+  mainLanguage: 'JavaScript, TypeScript'
+};
