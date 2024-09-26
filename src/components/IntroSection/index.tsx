5e442ab1cb3cf508f3811283fbe06236308a4bdd
krysthecoder <krysthopher@gmail.comm>
2024-09-25 21:11:04 -0600

    reformat: enhanced intro section layout

--- src/components/IntroSection/index.tsx
+++ src/components/IntroSection/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import Portrait from './../../assets/Portrait.jpeg';
+import { utilsData } from '../../utils/utilsData';
 
 function IntroSection() {
   return (
-    <div>
-      <div>Dark/Light</div>
-      <h1>Hola amigos como estan?</h1>
+    <div className="h-screen">
+      <div className="w-8/12 h-screen items-center justify-center mx-auto flex flex-col gap-2">
+        <div className="mx-auto ">
+          <img
+            src={Portrait}
+            alt="Self-Portrait"
+            className="rounded-full w-52 h-52 transition-all duration-300 cursor-pointer filter grayscale-0 hover:grayscale"
+          />
+        </div>
+        <p className="text-7xl">Krysthopher Ruiz</p>
+        <p className="text-2xl">
+          {utilsData.mainLanguage.split(' ').join(' and ')} developer.
+        </p>
+        <ul className="flex text-sm gap-2">
+          <li>About</li>
+          <li>Skills</li>
+          <li>Portfolio</li>
+          <li>Services</li>
+          <li>Certifications</li>
+          <li>Download CV</li>
+          <li>Contact me</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/utils/utilsData.tsx
+++ src/utils/utilsData.tsx
@@ -1,6 +1,14 @@
 // Here is all the content of my website, links, files links, etc.
 
-export const utilsData: object = {
+interface utilsData {
+  devName: string;
+  mainLanguage: string;
+  introContent: string;
+}
+
+export const utilsData: utilsData = {
   devName: 'Krysthopher Ruiz',
-  mainLanguage: 'JavaScript, TypeScript'
+  mainLanguage: 'JavaScript TypeScript',
+  introContent:
+    '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
 };
